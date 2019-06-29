06959079069993c7751aa173929400da8f7b7947
鈴木裕貴 <ytk.mac@suzukiyukinoMacBook-Pro.local>
2019-06-29 15:03:17 +0900

    integrated portfolio action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @blog = Blog.all
+    @posts = Blog.all
     @skills = Skill.all
   end
 
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,7 @@
 class PortfoliosController < ApplicationController
+
+  def index
+    @potrfolio_item = Portfolio.all
+  end
+
 end

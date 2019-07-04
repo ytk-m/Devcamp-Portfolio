8c7b43282a7d7f1746442680591e1815e52fca35
鈴木裕貴 <ytk.mac@suzukiyukinoMacBook-Pro.local>
2019-07-05 08:25:41 +0900

    implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -45,6 +45,19 @@ class PortfoliosController < ApplicationController
 
   end
 
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destriy/delete the record
+    @portfolio_item.destroy
+
+    #Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Redirect was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
 
 end

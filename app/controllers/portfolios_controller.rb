2651bc517af9479e351836c328349930e2804774
鈴木裕貴 <ytk.mac@suzukiyukinoMacBook-Pro.local>
2019-07-05 07:09:30 +0900

    implemmented rails edit and control bottun

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,9 +26,9 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
-    def update
+  def update
       @portfolio_item = Portfolio.find(params[:id])
-      
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -39,5 +39,12 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+
+  end
+
+
 
 end

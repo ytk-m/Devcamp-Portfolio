1c2039ef41260a324b7478d5438fb2c1fa6a849f
鈴木裕貴 <ytk.mac@suzukiyukinoMacBook-Pro.local>
2019-08-08 19:22:49 +0900

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+ has_many :technologies
+ include Placeholder
  validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -6,5 +8,13 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> {where(subtitle: "Ruby on Rails")}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||=Placeholder.image_generator(height: "600", width:"400")
+    self.thumb_image ||=Placeholder.image_generator(height: "350", width:"250")
+  end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -50,3 +50,14 @@ end
 puts "9 portfolio items created"
 
 
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
+
+
+
+
